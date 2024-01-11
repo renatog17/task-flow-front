4a9dec41d269780f52ac12eee1b97fe969fd0907
Renato Oliveira <renatog.oliveira17@gmail.com>
2024-01-10 23:56:57 -0300

    interface entity strategy

--- src/app/list-view/list-view.component.ts
+++ src/app/list-view/list-view.component.ts
@@ -12,7 +12,7 @@ export class ListViewComponent implements OnInit{
   
   @Input() list: any;
   tasks: Task[] = []
-  imageUrl = "assets/reticencias.png";
+  imageUrl = "assets/trash.png";
 
   constructor(private taskService: TaskService){
 
